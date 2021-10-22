7ebfb2b4e0b92d0f56ab72dbcf2ec030cc7cdcfd
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-22 17:17:08 +0100

    edited the class

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,18 +8,21 @@ end
 
 
 RSpec.describe Card do
-    before do
-    # before(:example) do
-        puts 'hellooooooooooooo'
-        @card = Card.new('Ace', 'Sapdes')
+    def card
+        Card.new('Ace', 'Sapdes')
     end
+    # before do
+    # # before(:example) do
+    #     puts 'hellooooooooooooo'
+    #     @card = Card.new('Ace', 'Sapdes')
+    # end
 
 
     it 'has a rank' do
-        expect(@card.rank).to eq('Ace')
+        expect(card.rank).to eq('Ace')
     end
 
     it 'has a suit' do
-        expect(@card.suit).to eq('Sapdes')
+        expect(card.suit).to eq('Sapdes')
     end
 end
