31bb6ead23a0301c63af42ed4fa2c255aa0e60b9
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-25 17:03:13 +0100

    even method

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,3 +1,19 @@
-Rspec.describe '#even? method' do
-    
+RSpec.describe '#even? method' do
+    # it 'should return true if number is even' do       
+    # end
+
+    # it 'shoudl return false if number is odd' do   
+    # end
+
+    describe 'with even number' do
+        it 'should return true' do
+            expect(4.even?).to eq(true)
+        end
+    end
+
+    describe 'with odd number' do
+        it 'should return false' do
+            expect(5.even?).to eq(false)
+        end
+    end
 end
