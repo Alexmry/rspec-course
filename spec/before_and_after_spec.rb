68084f3b95e3d8a2c7e96b61410bf9044ab50230
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-25 18:43:56 +0100

    before and after hooks

--- spec/before_and_after_spec.rb
+++ spec/before_and_after_spec.rb
@@ -1,3 +1,17 @@
 RSpec.describe 'before and after hooks' do
-    
+    before(:example) do
+        puts 'Before example'
+    end
+
+    after(:example) do
+        puts 'after example'
+    end
+
+    it 'is the juts random exemple' do
+        expect(5*4).to eq(20)
+    end
+
+    it 'is the juts another random exemple' do
+        expect(3-2).to eq(1)
+    end
 end
