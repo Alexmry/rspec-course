4c20d283d0394fd9b558e0766bf50d3ff57a5df3
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-26 09:10:23 +0100

    Nested Hooks

--- spec/nested_hooks_spec.rb
+++ spec/nested_hooks_spec.rb
@@ -1,3 +1,23 @@
 RSpec.describe 'Nested Hooks' do
-    
+    before(:context) do
+       puts 'before context' 
+    end
+
+    before(:example) do
+        puts 'before example' 
+    end
+
+    it 'does basic math' do
+        expect(1+1).to eq(2)
+    end
+
+    context 'with condition A' do
+        before(:context) do
+            puts 'before context' 
+        end
+     
+        before(:example) do
+            puts 'before example' 
+        end 
+    end
 end
