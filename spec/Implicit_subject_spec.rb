4439432272ebe8c6aa5058a81181bbd98a2d64e2
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-26 18:03:24 +0100

    Implicit Subject

--- spec/Implicit_subject_spec.rb
+++ spec/Implicit_subject_spec.rb
@@ -1,3 +1,16 @@
 RSpec.describe Hash do
-    let(:my_hash){ {} }
+    # let(:my_hash){ {} }
+    # let(:subject) { Hash.new }
+
+    it 'should start off empty' do
+        # puts subject
+        # puts subject.class
+        expect(subject.length).to eq(0)
+        subject[:some_key] = 'some value'
+        expect(subject.length).to eq(1)
+    end
+
+    it 'is isolated between examples' do
+        expect(subject.length).to eq(0)
+    end
 end
