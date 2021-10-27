a2ae71836e339dbbab40858db7931c490a980f12
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-27 18:18:04 +0100

    Explicit Subject

--- spec/​array_spec.rb
+++ spec/​array_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe Array do
     it 'should be an empty array' do
         expect(subject.length).to eq(0)
+        subject << 'hello'
+        expect(subject.length).to eq(1)       
     end
 end
