5ef3c7efac1a7cc34c15cd7e37f56a8df349ba40
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-28 10:56:19 +0100

    shared example

--- spec/shared_examples_spec.rb
+++ spec/shared_examples_spec.rb
@@ -1,14 +1,26 @@
 
 RSpec.describe Array do
     subject { [1, 2, 3] }
+
+    it 'return the number of items' do
+        expect(subject.length).to eq(3)
+    end
 end
 
 RSpec.describe String do
     subject { 'abc' }
+
+    it 'return the number of items' do
+        expect(subject.length).to eq(3)
+    end
 end
 
 RSpec.describe Hash do
     subject { {a: 1, b: 2, c: 3} }
+
+    it 'return the number of items' do
+        expect(subject.length).to eq(3)
+    end
 end
 
 class SausageLink
