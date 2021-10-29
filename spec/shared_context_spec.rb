b85d0b88d9391bdce6da117008787d00d6f2e37b
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-29 19:18:31 +0100

    Shared Context with include_context

--- spec/shared_context_spec.rb
+++ spec/shared_context_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe 'first example group' do
         @foods << 'burger'
         expect(@foods.length).to eq(1)
     end
+
+    it 'can reuse instance variables accross different examples' do
+        expect(@foods.length).to eq(0)
+    end
+
+    it 'can use shared helper method' do
+        expect(some_helper_method).to eq(5)
+    end
+end
+
+RSpec.describe 'second example' do
+    include_context 'common'
+
+    it 'can share shared variable' do
+        expect(some_variable).to eq([1, 2, 3])
+    end
 end
