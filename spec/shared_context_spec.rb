68748fa9915b45e28eb50323ca411f16e27bd404
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-29 19:08:02 +0100

    Shared Context with include_context

--- spec/shared_context_spec.rb
+++ spec/shared_context_spec.rb
@@ -2,4 +2,14 @@ RSpec.shared_context 'common' do
     before do
         @food = []
     end
+
+    def some_helper_method
+        5
+    end
+
+    let(:some_variable) { [1, 2, 3] }
+end
+
+RSpec.describe 'first example group' do
+    
 end
