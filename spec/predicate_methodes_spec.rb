8c128f78c529bef7b0d073c00e044c7d1c4f28e4
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-02 17:38:44 +0000

    predicate methods

--- spec/predicate_methodes_spec.rb
+++ spec/predicate_methodes_spec.rb
@@ -1,12 +1,23 @@
-puts 0.zero?
-puts 15.zero?
+# puts 0.zero?
+# puts 15.zero?
 
-puts 2.even?
-puts 3.even?
+# puts 2.even?
+# puts 3.even?
 
-puts 11.odd?
-puts 16.odd?
+# puts 11.odd?
+# puts 16.odd?
 
-puts [].empty?
-puts [1].empty?
+# puts [].empty?
+# puts [1].empty?
+
+RSpec.describe 'predicate methods and predicate matchers' do
+    it 'can be tested with plain ruby methods' do
+        result = 16/2
+        expect(result.even?). to eq(true)
+    end
+
+    it 'can be tested with predicate matchers' do
+        expect(16 / 2).to be_even
+    end
+end
 
