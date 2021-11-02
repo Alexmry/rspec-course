832c57565807bc54b56a0e360d50b8427e23d90f
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-02 12:18:26 +0000

    not to method

--- spec/not_to_method_spec.rb
+++ spec/not_to_method_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe 'no to method' do
     it 'checks thattwo values does not match' do
-        expect(5). not_to eq(5)
+        expect(5). not_to eq(10)
+        expect('Hello').not_to eq('hello')
+        expect([1, 2]).not_to eq([1, 2, 3])
     end
 end
