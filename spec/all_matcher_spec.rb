ceb5cd35c4a412a26a52dbfbeae76d2c0d352a2e
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-02 18:22:09 +0000

    all matcher

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe 'all matcher' do
     #        expect(val).to be_odd 
     #    end
         expect([5, 7, 9]).to all(be_odd)
+        expect([2, 4, 6]).to all(be_even)
+        expect([[],[],[]]).to all(be_empty)
+        expect([0, 0, 0]).to all(be_zero)
+        expect([5, 7, 9]).to all(be < 10)
     end
 end
