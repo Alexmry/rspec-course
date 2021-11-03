56691a77a9f31ea41fd1cd88f53bae3b96122df6
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-03 08:54:35 +0000

    start with and end with matchers

--- spec/satrt_with_and_end_with_matchers_spec.rb
+++ spec/satrt_with_and_end_with_matchers_spec.rb
@@ -1,5 +1,8 @@
 RSpec.describe 'start with and end with matchers' do
     describe 'caterpillar' do
-        
+        it 'should check for a substring at the beginning or end' do
+            expect(subject).to start_with('cat')
+            expect(subject).to end_with('pillar')
+        end 
     end
 end
