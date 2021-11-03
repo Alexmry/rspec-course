8edc5cbf577207af9a3e6d935070198a2f9744d1
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-03 09:03:26 +0000

    start with and end with matchers

--- spec/satrt_with_and_end_with_matchers_spec.rb
+++ spec/satrt_with_and_end_with_matchers_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe 'start with and end with matchers' do
         it { is_expected.to start_with('cat') }
         it { is_expected.to end_with('pillar') }
     end
+
+    describe [:a, :b, :c, :d] do
+        it 'should checks for the elements at the end or the beginning ogf the array' do
+            expect(subject).to start_with(:a)
+            expect(subject).to start_with(:a, :b)
+            expect(subject).to start_with(:a, :b, :c)
+            
+            expect(subject).to end_with(:d)
+            expect(subject).to end_with(:c, :d)
+
+        end
+
+        it { is_expected.to start_with(:a, :b)}
+    end
 end
