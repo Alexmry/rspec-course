88ba2302cb8d72c70fd74f2f874008470b15bfaa
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-03 08:48:04 +0000

    contain exactly

--- spec/contain_exactly matcher_spec.rb
+++ spec/contain_exactly matcher_spec.rb
@@ -8,4 +8,7 @@ RSpec.describe 'contain exactly matcher' do
            expect(subject).to contain_exactly(2, 1, 3)
        end
    end
+
+   it { is_expected.to contain_exactly(1, 2, 3) }
+   it { is_expected.to contain_exactly(3, 2, 1) }
 end
