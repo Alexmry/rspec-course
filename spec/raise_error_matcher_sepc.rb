b64c5c10cda87d5e4a04359aaf73ff820df1ea19
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-03 11:49:04 +0000

    raise error matcher

--- spec/raise_error_matcher_sepc.rb
+++ spec/raise_error_matcher_sepc.rb
@@ -8,6 +8,8 @@ RSpec.describe 'raise error matcher' do
     #     expect { some_method }.to raise_error
     # end
 
+    class CustomError < StandardError; end
+
     it 'can check for a specific error being raised' do
         expect { some_method }.to raise_error(NameError)
         expect { 10/0 }.to raise_error(ZeroDivisionError)
