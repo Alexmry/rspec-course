3c41a45eab41e472b1dc27d788268fc4db8b3b69
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 19:52:51 +0000

    satisfy matcher

--- spec/satisfy_matcher_spec.rb
+++ spec/satisfy_matcher_spec.rb
@@ -1,7 +1,14 @@
 RSpec.describe 'satify matcher' do
     subject { 'racecar' }
+    # subject { 'racecars' }
 
     it' is a palindrome' do
-        expect(subject).to satisfy { |value| value ==value.reverse }
+        expect(subject).to satisfy { |value| value == value.reverse }
+    end
+
+    it 'can accept a custom error message' do
+        expect(subject).to satisfy('to be a palindrome') do |value|
+            value == value.reverse
+        end
     end
 end
