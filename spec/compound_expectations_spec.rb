41c8bb9c3476e2503f2921e3985bb2d7fe80439e
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 20:28:00 +0000

    compound expectations

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe 25 do
 
         expect(subject).to be_odd.and be > 20
     end
+
+    it { is_expected.to be_odd.and be > 20 }
+end
+
+RSpec.describe 'caterpillar' do
+    it 'supports multiple matchers on a single line' do
+        expect(subject).to start_with('cat').and end_with('pillar')
+    end
+
+    it { is_expected.to start_with('cat').and end_with('pillar') }
 end
