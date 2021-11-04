710a72130a09f2bc845da531cb0163245b56be2f
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 19:24:39 +0000

    matcher

---
+++ spec/respond_to_matcher_spec.rb
@@ -0,0 +1,19 @@
+class HotChocolate
+    def drink
+        'Delicious'
+    end
+
+    def discard
+        'PLOP!'
+    end
+
+    def purchase(number)
+        "Awesome, I just purchased #{number} more hot chocolate beberages!"
+    end
+end
+
+class Coffee
+    def drink; end
+    def discard; end
+    def purchase(number); end
+end
