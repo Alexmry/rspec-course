662e96dc1e4e00db20c514468aa6199e66628060
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 19:40:00 +0000

    matcher

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -19,7 +19,17 @@ end
 # end
 
 RSpec.describe HotChocolate do
-    it 'confirmes that an object can respond to a method' do
-        expect(subject).to respond_to(drink)
+    it 'confirms that an object can respond to a method' do
+        expect(subject).to respond_to(:drink)
+        expect(subject).to respond_to(:drink, :discard)
+        expect(subject).to respond_to(:drink, :discard, :purchase)
     end
+
+    it 'confirms as well that an object can respond to a method with an argument' do
+        expect(subject).to respond_to(:purchase)
+        expect(subject).to respond_to(:purchase).with(1).arguments
+    end
+
+    it { is_expected.to respond_to(:purchase, :discard) }
+    it { is_expected.to respond_to(:purchase).with(1).arguments }
 end
