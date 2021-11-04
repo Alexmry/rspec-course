073d051483ad3865a2de4316646986851f657597
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 19:31:07 +0000

    matcher

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -12,8 +12,14 @@ class HotChocolate
     end
 end
 
-class Coffee
-    def drink; end
-    def discard; end
-    def purchase(number); end
+# class Coffee
+#     def drink; end
+#     def discard; end
+#     def purchase(number); end
+# end
+
+RSpec.describe HotChocolate do
+    it 'confirmes that an object can respond to a method' do
+        expect(subject).to respond_to(drink)
+    end
 end
