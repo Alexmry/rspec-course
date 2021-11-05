ebfefd0fcff01f92db82a00b8d9b1619f5ab5934
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-05 16:24:57 +0000

    double

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -1,7 +1,15 @@
 RSpec.describe 'random double' do
     it 'only allows defined method to be invoked' do
-        stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch')
+        # stuntman = double("Mr. Danger", fall_off_ladder: 'Ouch', light_on_fire: true)
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+        # expect(stuntman.light_on_fire).to eq(true)
+        
+        # stuntman = double("Mr. Danger")
+        # allow(stuntman).to receive(:fall_off_ladder)
+        # expect(stuntman.fall_off_ladder).to be_nil
 
-        stuntman.fall_off_ladder
+        stuntman = double("Mr. Danger")
+        allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        expect(stuntman.fall_off_ladder).to eq('Ouch')
     end
 end
