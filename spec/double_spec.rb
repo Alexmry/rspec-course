245e2426d532b04906c55dc87e079359e7811107
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-05 16:28:11 +0000

    double

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -8,8 +8,13 @@ RSpec.describe 'random double' do
         # allow(stuntman).to receive(:fall_off_ladder)
         # expect(stuntman.fall_off_ladder).to be_nil
 
+        # stuntman = double("Mr. Danger")
+        # allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+
         stuntman = double("Mr. Danger")
-        allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
         expect(stuntman.fall_off_ladder).to eq('Ouch')
+        expect(stuntman.light_on_fire).to eq(true)
     end
 end
