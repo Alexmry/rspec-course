3c5bccf7fb93ac8e30a45ca8040ae049b78ba898
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-08 10:57:45 +0000

    allow method

---
+++ spec/allow_method_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'allow method review' do
+    it 'can customize return value for methods on doubles' do
+        calculator = double
+        allow(calculator).to receive(:add).and_return(15)
+        # expect(calculator.add).to be_nil
+        expect(calculator.add).to eq(15)
+        expect(calculator.add(3)).to eq(15)
+        expect(calculator.add(-2, -3, -5)).to eq(15)
+        expect(calculator.add('hello')).to eq(15)
+    end
+
+    it 'can stub one or more method on a real object' do
+        arr = [1, 2, 3]
+        expect(arr.sum).to eq(6)
+    end
+end
