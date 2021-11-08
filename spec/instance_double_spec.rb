258a371f4d28bc89e32dbfd72c364ec5f3823f21
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-08 16:36:01 +0000

    instance double

--- spec/instance_double_spec.rb
+++ spec/instance_double_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Person do
             expect(person.a).to eq("hello")
         end
     end
+
+    describe 'instance double' do
+        it 'can only implement methods that are defined on the class' do
+            # person = instance_double(Person, a: 'hello', b: 20)
+
+            person = instance_double(Person)
+        end
+    end
 end
