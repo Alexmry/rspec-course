7c0d3153bd8f7c5a15dacda6c2b22e761a8ba1ed
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-09 10:01:39 +0000

    class double

--- spec/class_double_spec.rb
+++ spec/class_double_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe CardGame do
     it' can only implement class method that are define on a class' do
         # class_double(Deck, build: ['Ace', 'Queen'], shuffle: ['Queen', 'Ace'])
         # class_double(Deck, build: ['Ace', 'Queen'])
-        class_double('Deck', build: ['Ace', 'Queen'])
+        # class_double('Deck', build: ['Ace', 'Queen'])
+        deck_klass = class_double('Deck', build: ['Ace', 'Queen']).as_stubbed_const
     end
 end
