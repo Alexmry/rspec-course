779dda6515bf9925403d0bebb7994a28134a01a6
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-09 14:18:29 +0000

    spies

--- spec/spies_sepc.rb
+++ spec/spies_sepc.rb
@@ -10,4 +10,11 @@ RSpec.describe 'animal' do
     it 'reset between example' do
         expect(animal).not_to have_received(:eat_food)
     end
+
+    it 'retains the same fonctionality of a regular double' do
+        animal.eat_food
+        animal.eat_food
+        animal.eat_food('suishi')
+        expect(animal).to have_received(:eat_food).exactly(3).times
+    end
 end
