59733b7a5a46ea1afff4941ee1b3b56fcc2b4535
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-09 10:16:12 +0000

    class double

--- spec/class_double_spec.rb
+++ spec/class_double_spec.rb
@@ -1,8 +1,8 @@
-# class Deck
-#     def self.build
-#         # business logic to build a whole bunch of cards
-#     end
-# end
+class Deck
+    def self.build
+        # business logic to build a whole bunch of cards
+    end
+end
 
 class CardGame
     attr_reader :cards
@@ -16,6 +16,11 @@ RSpec.describe CardGame do
         # class_double(Deck, build: ['Ace', 'Queen'], shuffle: ['Queen', 'Ace'])
         # class_double(Deck, build: ['Ace', 'Queen'])
         # class_double('Deck', build: ['Ace', 'Queen'])
-        deck_klass = class_double('Deck', build: ['Ace', 'Queen']).as_stubbed_const
+        # deck_klass = class_double('Deck', build: ['Ace', 'Queen']).as_stubbed_const
+        deck_klass = class_double(Deck, build: ['Ace', 'Queen']).as_stubbed_const
+
+        expect(deck_klass).to receive(:build)
+        subject.start
+        expect(subject.cards).to eq(['Ace', 'Queen'])
     end
 end
