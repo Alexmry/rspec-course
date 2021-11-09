76c59efef815e7e27e94d0a2f60a387d36709f9d
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-09 11:47:03 +0000

    spies

--- spec/spies_sepc.rb
+++ spec/spies_sepc.rb
@@ -2,7 +2,8 @@ RSpec.describe 'animal' do
     let(:animal) { spy('animal') }
 
     it 'confirms that a message has been received' do
-        expect(animal).to receive(:eat_food)
         animal.eat_food
+        expect(animal).to have_received(:eat_food)
+        expect(animal).not_to have_received(:eat_human)
     end
 end
